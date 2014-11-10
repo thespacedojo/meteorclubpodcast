1d962789848edcbf016b64ecedd9d11fd684b49e
Amanda Saffer <amandasaffer@gmail.com>
2014-11-10 00:55:15 -0600

    Questions page implementation

--- client/views/questions.js
+++ client/views/questions.js
@@ -2,18 +2,18 @@
 var guestDetails = [
 	{
 		name: 'Sacha Greif',
-		img: '/images/josh.png',
-		url: 'http://google.com'
+		img: '/images/sacha.png',
+		url: 'http://sachagreif.com'
 	}, 
 	{
 		name: 'Arunoda Susiripala',
-		img: '/images/josh.png',
-		url: 'http://google.com'
+		img: '/images/arunoda.png',
+		url: 'http://meteorhacks.com'
 	}, 
 	{
 		name: 'Chris Mather',
-		img: '/images/josh.png',
-		url: 'http://google.com'
+		img: '/images/chrismather.png',
+		url: 'http://eventedmind.com'
 	},
 	{
 		name: 'Lorem Ipsum',
@@ -25,22 +25,22 @@ var guestDetails = [
 // array for video archive details
 var videoDetails = [
 	{
-		featured: 1,
-		month: 'Join us live on November 28th!',
+		upcoming: 1,
+		month: 'November 2014',
 		thumbnail: 'http://img.youtube.com/vi/VFgci7-xVc8/mqdefault.jpg',
-		url: 'http://google.com',
-		description: 'The next Meteor Q&A session will air live on November 28th at 6pm CST. Join Josh Owens and Chris Mathers as they talk about lorem ipsum dolor sit amet and answer your questions.'
+		url: '/transcript',
+		description: 'The next Meteor Q&A session will air live November 28th at 6pm CST. Join Josh Owens and Chris Mathers as they talk about lorem ipsum dolor sit amet and answer your questions.'
 	},
 	{
 		month: 'October 2014',
 		thumbnail: 'http://img.youtube.com/vi/LqWbXQDXHC0/mqdefault.jpg',
-		url: 'http://google.com',
-		description: 'Special guest Arunoda Susiripala talks with Josh about lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione nemo, voluptates maiores, soluta deleniti minima at, inventore officiis in dolorem excepturi harum ducimus.'
+		url: '/transcript',
+		description: 'Special guest Arunoda Susiripala talks with Josh about lorem draco dormiens nunquam titillandus ipsum dolor sit amet, consectetur adipisicing elit. Ratione nemo, voluptates maiores, soluta deleniti minima at, inventore officiis in dolorem excepturi harum ducimus.'
 	},
 	{
 		month: 'September 2014',
 		thumbnail: 'http://img.youtube.com/vi/VFgci7-xVc8/mqdefault.jpg',
-		url: 'http://google.com',
+		url: '/transcript',
 		description: 'Special guest Sacha Greif talks with Josh about lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione nemo, voluptates maiores, soluta deleniti minima at, inventore officiis in dolorem excepturi harum ducimus.'
 	},
 ];
@@ -48,4 +48,10 @@ var videoDetails = [
 Template.questions.helpers({
 	guests: guestDetails,
 	videos: videoDetails
+});
+
+Template.transcript.events({
+	'click .goBack': function() {
+		window.history.back();
+	}
 });
