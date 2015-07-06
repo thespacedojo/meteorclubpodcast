864bdeb944f65b7458b671f7a6daa2c90c642ee4
Josh Owens <joshua.owens@gmail.com>
2015-07-06 11:01:22 -0400

    Fix SSL issues

--- collections/episodes.js
+++ collections/episodes.js
@@ -1,6 +1,12 @@
 // Create episodes collection
 Episodes = new Mongo.Collection('episodes');
 
+Episodes.helpers({
+  statelessDownload: function() {
+    return this.download.replace(/http:/, '');
+  }
+});
+
 Episodes.allow({
   insert: function(userId, doc) {
     return Roles.userIsInRole(userId, 'admin');
